f3fd043a4bfbc28b7e7b1711968962b1f6e38345
AkiraTokashiki <79854744+AkiraTokashiki@users.noreply.github.com>
2022-07-04 15:58:29 -0300

    Update Resultado.jsx

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,20 +1,56 @@
 import styled from "@emotion/styled"
 
- const Resultado = ({resultado}) => {
+const Result = styled.div`
+  color: white;
+  font-size: 'Lato', sans-serif;
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px ;
+`
+const Imagen = styled.img`
+  display: block;
+  width: 120px;
+
+`
+
+
+const Texto = styled.p`
+  font-size: 18px;
+  span { 
+    font-weight: 700;
+  }
+  
+`
+const Precio = styled.p`
+  font-size: 24px;
+  span { 
+    font-weight: 700;
+  }
+`
+
+ const Resultado = ({resultado}) => { 
   const  { PRICE, HIGHDAY, LOWDAY, 
-  CHANGEPCT24HOURS, IMAGEURL, LASTUPDATE } = resultado
+    CHANGEPCT24HOURS, IMAGEURL, LASTUPDATE } = resultado
   
   return (
-   <div> 
-    <p>El precio es de: <span> {PRICE} </span></p>
-    <p>El precio mas alto del dia:  <span> {HIGHDAY} </span></p>
-    <p>El precio mas bajo del dia: <span> {LOWDAY} </span></p>
-    <p>Variacopm ultimas 24hs: <span> {CHANGEPCT24HOURS} </span></p>
-    <p>Ultima actualizacion: <span> {LASTUPDATE} </span></p>
-    
+   <Result> 
+      <Imagen
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="imagen cripto"
+      />
+    <div> 
+      <Precio>El precio es de: <span> {PRICE} </span></Precio>
+      <Texto>El precio mas alto del dia:  <span> {HIGHDAY} </span></Texto>
+      <Texto>El precio mas bajo del dia: <span> {LOWDAY} </span></Texto>
+      <Texto>Variacion ultimas 24hs: <span> {CHANGEPCT24HOURS} </span></Texto>
+      <Precio>Ultima actualizacion: <span> {LASTUPDATE} </span></Precio>
+    </div>
 
 
-  </div> 
+  </Result> 
   )
 }
+ 
 export default Resultado
